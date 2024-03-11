f5024090e0b383756061b7c28bec792707e2fe24
AceYang123 <leonisita@126.com>
2024-03-11 19:13:36 -0400

    Add files via upload

--- src/LoadTest.java
+++ src/LoadTest.java
@@ -86,19 +86,21 @@ public class LoadTest {
 		String[] body;
 		String command;
 		JSONObject response;
+		String servers;
 
 		rand = new Random();
-
+		long startTime = System.currentTimeMillis(); // Start timer
 		try {
-      
-     
+
+
 			for(int i=0;i<numRequests;i++){
         // Modify to print helpful information if you'd like
         if (i % 100 == 0) {
           System.out.println(i);
-        } 
+        }
+		//不知道有啥用，先comment了
         // You probably want to add timers to take into account the time that has passed already.
-        Thread.sleep(1000/maxRequestsPerSecond);
+        //Thread.sleep(1000/maxRequestsPerSecond);
 				//只会生成0-9的id
 				int randomDigit = rand.nextInt(10);
 				String id = Integer.toString(randomDigit);
@@ -115,42 +117,48 @@ public class LoadTest {
 				body[2] = name;
 				body[3] = randomString(10);
 
-//				int server = rand.nextInt(3);
-				int server = 2;
+				int server = rand.nextInt(3);
+				port = 14000;
 				switch (server) {
 					case 0 -> {
 						//user: 14001
-						port = 14001;
+						servers = "/user";
 						body[4] = randomString(10);
-						System.out.println(Arrays.toString(body));
+						//System.out.println(Arrays.toString(body));
 						response = readUserJSON(body);
-						System.out.println(response.toString());
+						//System.out.println(response.toString());
 					}
 					case 1 -> {
 						// product: 15000
-						port = 15000;
+						servers = "/product";
 						body[4] = Integer.toString(rand.nextInt(100));
 						body[5] = Integer.toString(rand.nextInt(100));
+						//System.out.println(Arrays.toString(body));
+
 						response = readProductJSON(body);
-						System.out.println(response.toString());
+						//System.out.println(response.toString());
 					}
 					default -> {
 						//OrderService 14000
-						port = 14000;
+						servers = "/order";
 						body[0] = "place order";
 						body[1] = id;
 						body[2] = Integer.toString(rand.nextInt(10));
 						body[3] = Integer.toString(rand.nextInt(100));
+						//System.out.println(Arrays.toString(body));
+
 						response = readOrderJSON(body);
-						System.out.println(response.toString());
+						//System.out.println(response.toString());
 					}
 				}
 				int randomInt = rand.nextInt(2); // Generates a random integer from 0 (inclusive) to 100 (exclusive)
 				//put
+				//System.out.println("get or post: " + randomInt);
 				if(randomInt == 0){
-					put("http://"+host+":"+port, response.toString());
+					post("http://"+host+":"+port+servers, response.toString());
 				}else{
-					get("http://"+host+":"+port+"/"+id);
+					get("http://"+host+":"+port+servers+"/"+id);
+					//System.out.println("http://"+host+":"+port+servers+"/"+id);
 				}
 				// get("http://mcs.utm.utoronto.ca");
 				// get("http://localhost:8080/89M6VVVP7369R1VEPSP0");
@@ -158,13 +166,16 @@ public class LoadTest {
 		} catch (Exception e){
 			e.printStackTrace();
 		}
+		long endTime = System.currentTimeMillis(); // End timer
+		long duration = endTime - startTime;
+		System.out.println("Request Duration: " + duration + " ms");
 	}
 
-	public static void put(String uri, String requestBody) throws Exception {
+	public static void post(String uri, String requestBody) throws Exception {
 		HttpClient client = HttpClient.newHttpClient();
 		HttpRequest request = HttpRequest.newBuilder()
 				.uri(URI.create(uri))
-				.PUT(HttpRequest.BodyPublishers.ofString(requestBody))
+				.POST(HttpRequest.BodyPublishers.ofString(requestBody))
 				.build();
 	
 		HttpResponse<String> response =
@@ -182,7 +193,10 @@ public class LoadTest {
 	
 	    HttpResponse<String> response =
 	          client.send(request, HttpResponse.BodyHandlers.ofString());
-	
-	    // System.out.println(response.body());
+		System.out.println("HTTP Status Code: " + response.statusCode());
+		System.out.println("Response Body: " + response.body());
+
+
+		// System.out.println(response.body());
 	}
 }
